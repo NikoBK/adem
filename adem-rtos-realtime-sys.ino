c196ba626c6159f2d9627d4f97f760834856a2c3
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-11-02 23:01:09 +0100

    verbose setting

--- adem-rtos-realtime-sys.ino
+++ adem-rtos-realtime-sys.ino
@@ -1,12 +1,17 @@
 int increment = 10;
+bool verbose = false;
 
 void setup() {
 	Serial.begin(115200);
-  Serial.println("The increment variable is: " + String(increment));
+  if (verbose) {
+    Serial.println("The increment variable is: " + String(increment));
+  }
 }
 
 void loop() {
 	  increment += 10;
     delay(500); // Wait 500ms
-    Serial.println("Increment variable is now: " + String(increment));
+    if (verbose) {
+      Serial.println("Increment variable is now: " + String(increment));
+    }
 }
