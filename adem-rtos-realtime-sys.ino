f4177cd5996540d39dc53f1e983c6436714addbf
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-11-02 23:28:40 +0100

    single core dual thread/task test

--- adem-rtos-realtime-sys.ino
+++ adem-rtos-realtime-sys.ino
@@ -12,6 +12,7 @@ static const BaseType_t app_cpu = 1;
 #endif
 
 int delayMS = 1000;
+bool verbose = false;
 
 // Create a new task.
 void runDemoTask(void *parameter) {
@@ -20,7 +21,19 @@ void runDemoTask(void *parameter) {
     // Use vTaskDelay instead of delay() as it is nonblocking and works on freeRTOS.
     // portTICK refers to one of the hardware ticktimers (clocks).
     vTaskDelay(delayMS / portTICK_PERIOD_MS);
-    Serial.println("Tick!");
+    if (verbose) {
+      Serial.println("<Task1>: Tick!");
+    }
+  }
+}
+
+// Try another task, consec. with Demo Task 1
+void runDemoTaskTwo(void *parameter) {
+  while(1) {
+    vTaskDelay(delayMS / portTICK_PERIOD_MS);
+    if (verbose) {
+      Serial.println("<Task2>: Tick!");
+    }
   }
 }
 
@@ -37,6 +50,15 @@ void setup() {
         1,                 // Task prioerity (0 to configMAX_PRIORITIES - 1)
         NULL,              // Task handle
         app_cpu);          // Run on one core for demo purposes (ESP32 only)
+
+  xTaskCreatePinnedToCore(
+        runDemoTaskTwo,
+        "Demo Task Two",
+        1024,
+        NULL,
+        1, // Ensure that both tasks run by sharing priority
+        NULL,
+        app_cpu);
 }
 
 void loop() {
