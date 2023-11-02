b3dabc6e271936a0c753151b3290982b84b2bac9
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-11-02 22:59:12 +0100

    exercise 1

--- adem-rtos-realtime-sys.ino
+++ adem-rtos-realtime-sys.ino
@@ -1,23 +1,12 @@
-#include "Freenove_WS2812_Lib_for_ESP32.h"
-
-#define LEDS_COUNT  1 // Just one LED
-#define LEDS_PIN	8 // Pin 8 for CPIO8
-#define CHANNEL		0
-
-Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
-
-int delayMS = 2000;
+int increment = 10;
 
 void setup() {
-	strip.begin();
-	strip.setBrightness(3);	
+	Serial.begin(115200);
+  Serial.println("The increment variable is: " + String(increment));
 }
 
 void loop() {
-	  strip.setLedColorData(0, 255, 255, 255);
-    strip.show();
-    delay(delayMS);
-    strip.setLedColorData(0, 0, 0, 0);
-    strip.show();
-    delay(delayMS);
+	  increment += 10;
+    delay(500); // Wait 500ms
+    Serial.println("Increment variable is now: " + String(increment));
 }
