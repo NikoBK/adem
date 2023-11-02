c16a5c2c61933220c93f1483ac0707c04b2e6bfb
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-11-02 22:49:55 +0100

    simple esp32-c3 rgb led blink test

--- adem-rtos-realtime-sys.ino
+++ adem-rtos-realtime-sys.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Freenove_WS2812_Lib_for_ESP32.h"
 
+#define LEDS_COUNT  1 // Just one LED
+#define LEDS_PIN	8 // Pin 8 for CPIO8
+#define CHANNEL		0
+
+Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
+
+int delayMS = 2000;
+
+void setup() {
+	strip.begin();
+	strip.setBrightness(3);	
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+	  strip.setLedColorData(0, 255, 255, 255);
+    strip.show();
+    delay(delayMS);
+    strip.setLedColorData(0, 0, 0, 0);
+    strip.show();
+    delay(delayMS);
 }
