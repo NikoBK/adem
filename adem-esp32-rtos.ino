928c3d7fb4d92cf2c5c90e518871836dfa3eb84f
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-11-04 11:57:46 +0100

    exercise 1

--- adem-esp32-rtos.ino
+++ adem-esp32-rtos.ino
@@ -23,28 +23,23 @@ static const BaseType_t app_cpu = 0;
 static const BaseType_t app_cpu = 1;
 #endif
 
-int delayMS = 1000;
+int delayMS = 500;
 bool verbose = false;
 
+// Arbitrary number for exercise 1.
+int value = 10;
+
 // Create a new task.
-void runDemoTask(void *parameter) {
+void incrementTask(void *parameter) {
   // Our superloop.
   while(1) {
     // Use vTaskDelay instead of delay() as it is nonblocking and works on freeRTOS.
     // portTICK refers to one of the hardware ticktimers (clocks).
+    Serial.println("Current value is: " + String(value));
+    value += 10;
     vTaskDelay(delayMS / portTICK_PERIOD_MS);
     if (verbose) {
       Serial.println("<Task1>: Tick!");
-    }
-  }
-}
-
-// Try another task, consec. with Demo Task 1
-void runDemoTaskTwo(void *parameter) {
-  while(1) {
-    vTaskDelay(delayMS / portTICK_PERIOD_MS);
-    if (verbose) {
-      Serial.println("<Task2>: Tick!");
     }
   }
 }
@@ -55,22 +50,13 @@ void setup() {
 
   // Make sure our task only runs on one of the cores.
   xTaskCreatePinnedToCore(
-        runDemoTask,       // Function to be called for the task
-        "Demo Task",       // Task Name
+        incrementTask,       // Function to be called for the task
+        "Number Increment 1",       // Task Name
         1024,              // Stack size - 1kb (bytes in ESP32, words in vanilla freeRTOS)
         NULL,              // Parameter to pass to function
         1,                 // Task prioerity (0 to configMAX_PRIORITIES - 1)
         NULL,              // Task handle
         app_cpu);          // Run on one core for demo purposes (ESP32 only)
-
-  xTaskCreatePinnedToCore(
-        runDemoTaskTwo,
-        "Demo Task Two",
-        1024,
-        NULL,
-        1, // Ensure that both tasks run by sharing priority
-        NULL,
-        app_cpu);
 }
 
 void loop() {
